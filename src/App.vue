8a30d2f7baa0120459f3af0ff2c298e220af2ab2
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-11-10 17:19:07 -0300

    feat: init course repo

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Header from './components/Header.vue'
+  import Main from './components/Main.vue'
+
+  export default {
+    components: { Header, Main }
+  }
+</script>
+<template>
+  <Header />
+  <Main />
+</template>
